8a839aebd05ecb726dfd57eb9851752969d0c2df
Crowdbotics <team@crowdbotics.com>
2024-01-22 18:27:56 +0000

    Initial commit

---
+++ backend/foodstaff_45467/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for foodstaff_45467 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "foodstaff_45467.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "foodstaff-45467",
+  url: "https://foodstaff-45467.botics.co",
+  api: "https://foodstaff-45467.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
